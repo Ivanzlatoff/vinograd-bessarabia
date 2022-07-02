ca489daa7aa352534430c33f661a44af5a07398e
Ivan Zlatov <ivanzlatoff@gmail.com>
2022-07-02 21:29:50 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <hr />
+      <p>2022 Vinograd Bessarabii All rights reserved </p>
+      <p className="icons">
+        <AiFillInstagram />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
